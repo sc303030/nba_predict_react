596fec28e4ddbce7fdb13b8d127b093bb90249a8
sc303030 <lkh6035@naver.com>
2022-07-21 08:20:10 +0900

    22.07.21 | div 변경 효과 추가

--- src/Contents.tsx
+++ src/Contents.tsx
@@ -1,13 +1,14 @@
 import { Layout } from 'antd';
 import React from 'react';
 import SelectCategory from "./Select";
-import SearchInput from './SearchInput'
+import SearchInput from './SearchInput';
+import Information from "./Information";
 const {Content } = Layout;
 
 const contents: React.FC = () => (
       <Content>
         <section className="search-box-section bs">
-          <div className="search-box-child-1-div bs">
+          <div className="search-box-child-1-div bs ps">
             <div></div>
             <div></div>
             <div  className="search-box">
@@ -16,7 +17,9 @@ const contents: React.FC = () => (
             </div>
           </div>
         </section>
-        <div className="site-layout-content">Content</div>
+        <div className="site-layout-content ps">
+          <Information/>
+        </div>
       </Content>
   );
   
---
+++ src/Information.tsx
@@ -0,0 +1,49 @@
+
+import React from 'react';
+import "./information.css"
+const information: React.FC = () => (
+
+    <div className="wrapper">
+    <div className="container">
+        <input className="checkbox-all" id="all" type="radio" name="checkbox" checked={false}/>
+        <label htmlFor="all">all</label>
+        <input className="checkbox-js" id="js" type="radio" name="checkbox"/>
+        <label htmlFor="js">javascript</label>
+        <input className="checkbox-react" id="react" type="radio" name="checkbox"/>
+        <label htmlFor="react">react</label>
+        <input className="checkbox-html" id="html" type="radio" name="checkbox"/>
+        <label htmlFor="html">HTML/CSS</label>
+        <input className="checkbox-vue" id="vue" type="radio" name="checkbox"/>
+        <label htmlFor="vue">vue</label>
+        <input className="checkbox-angular" id="angular" type="radio" name="checkbox"/>
+        <label htmlFor="angular">angular</label>
+        <div className="seperator"></div>
+
+        <div className="cards">
+        <div className="project js"></div>
+        <div className="project html"></div>
+        <div className="project react"></div>
+        <div className="project vue"></div>
+        <div className="project angular"></div>
+        <div className="project js"></div>
+        <div className="project html"></div>
+        <div className="project react"></div>
+        <div className="project vue"></div>
+        <div className="project angular"></div>
+        <div className="project js"></div>
+        <div className="project html"></div>
+        <div className="project react"></div>
+        <div className="project vue"></div>
+        <div className="project angular"></div>
+        <div className="project js"></div>
+        <div className="project html"></div>
+        <div className="project react"></div>
+        <div className="project vue"></div>
+        <div className="project angular"></div>
+        </div>
+    </div>
+    </div>
+  );
+  
+  
+  export default information;
