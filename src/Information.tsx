f0c661022286fec57d25f1ee8088c97ad3f29fe2
sc303030 <lkh6035@naver.com>
2022-07-28 20:31:04 +0900

    220.7.28 | add typescript

--- src/Information.tsx
+++ src/Information.tsx
@@ -1,5 +1,5 @@
 
-import React, {useCallback} from 'react';
+import React, {useEffect } from 'react';
 import "./information.css"
 import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 import {
@@ -9,8 +9,25 @@ import {
 const Information: React.FC = () => {
 
     const searchValue = useRecoilValue<string>(selectValueState);
-    const changeCheckboxSelect = useCallback((): void => {
+    const changeCheckboxSelect = useEffect ((): void => {
+      const checkboxlist : NodeListOf<Element>  = document.getElementsByName('checkbox');
+      const project = document.getElementsByClassName("project");
+      const checkbox_yellow = document.getElementsByClassName('checkbox-all');
+      for (let  item of checkboxlist as any) {
+        item.checked = false;
+        const eClassName = item.getAttribute('class')?.split('-')[1]
+      }
+      // checkboxlist.forEach(node  => {
+      //   console.log(typeof(node))
+      //   const input = node as HTMLInputElement | null;
+      //   console.log(input.getAttribute('checked'))
+      //   const eClassName = node.getAttribute('class')?.split('-')[1]
+      //   // eClassName
+      // })
 
+      console.log(checkboxlist)
+      console.log(searchValue)
+      console.log(project)
       }, [searchValue]);
     const onChange = (e: CheckboxChangeEvent) => {
         console.log(`checked = ${e.target.checked}`);
@@ -21,15 +38,15 @@ const Information: React.FC = () => {
         <div className="container">
             <input className="checkbox-all" id="all" type="radio" name="checkbox" defaultChecked={true}/>
             <label htmlFor="all">all</label>
-            <input className="checkbox-yellow" id="yellow" type="radio" name="checkbox"/>
+            <input className="checkbox-yellow" id="yellow" type="radio" name="checkbox" defaultChecked={false}/>
             <label htmlFor="yellow">노란색</label>
-            <input className="checkbox-orange" id="orange" type="radio" name="checkbox"/>
+            <input className="checkbox-orange" id="orange" type="radio" name="checkbox" defaultChecked={false}/>
             <label htmlFor="orange">주황색</label>
-            <input className="checkbox-blue" id="blue" type="radio" name="checkbox"/>
+            <input className="checkbox-blue" id="blue" type="radio" name="checkbox" defaultChecked={false}/>
             <label htmlFor="blue">하늘색</label>
-            <input className="checkbox-green" id="green" type="radio" name="checkbox"/>
+            <input className="checkbox-green" id="green" type="radio" name="checkbox" defaultChecked={false}/>
             <label htmlFor="green">초록색</label>
-            <input className="checkbox-red" id="red" type="radio" name="checkbox"/>
+            <input className="checkbox-red" id="red" type="radio" name="checkbox" defaultChecked={false}/>
             <label htmlFor="red">빨간색</label>
             <div className="seperator"></div>
     
